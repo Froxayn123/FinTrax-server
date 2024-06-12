a9e7f9563e9aa13a9a59309a2f5408fe513143a4
Froxayn <rizkymyusuf123@gmail.com>
2024-06-12 17:59:48 +0700

    fix

--- middlewares/handlingError.js
+++ middlewares/handlingError.js
@@ -2,7 +2,7 @@ const handlingError = (error, req, res, next) => {
   console.log(error.message);
   res.status(500).json({
     message: "Something went wrong!",
-    error: error,
+    error: error.message,
   });
   next();
 };

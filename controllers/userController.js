982b0a9eb02579cb017ea9a93a312860d9027148
Froxayn <rizkymyusuf123@gmail.com>
2024-07-06 12:49:58 +0700

    revert: revert getUserController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,25 @@
 const db = require("../configs/connect");
 const fs = require("fs");
 
+const getUser = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+    if (!userId) return res.sendStatus(403);
+    const [results] = await db.query(
+      `SELECT users.fullname, users.username, avatars.avatar_url, users.balance, user_habits.habit_name, habit_recommendations.savings_percentage, habit_recommendations.wants_percentage, habit_recommendations.needs_percentage FROM users LEFT JOIN avatars ON users.id = avatars.user_id LEFT JOIN user_habits ON users.id = user_habits.user_id LEFT JOIN habit_recommendations ON user_habits.habit_name = habit_recommendations.name WHERE users.id = '${userId}';`
+    );
+
+    res.status(200).json({
+      payload: {
+        message: "User data has been successfully fetched",
+        payload: results,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 const editUser = async (req, res, next) => {
   try {
     const userId = req.userId;
@@ -52,4 +71,4 @@ const deleteAccount = async (req, res, next) => {
   }
 };
 
-module.exports = { editUser, changePhoto, deleteAccount };
+module.exports = { getUser, editUser, changePhoto, deleteAccount };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { editUser, changePhoto, deleteAccount } = require("../controllers/userController");
+const { editUser, changePhoto, deleteAccount, getUser } = require("../controllers/userController");
 const { verifyToken } = require("../middlewares/verifyToken");
 const { refreshToken } = require("../controllers/refreshToken");
 const upload = require("../middlewares/multer");
 const router = express();
 
+router.get("/users", verifyToken, getUser);
 router.get("/token", refreshToken);
 router.put("/users/profile", verifyToken, editUser);
 router.put("/users/profile/photo", verifyToken, upload, changePhoto);
